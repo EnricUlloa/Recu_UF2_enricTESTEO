aa91bb520e2cc539eb5e7c1b7214e7929d456a0e
eulloa23 <eulloa23@ilg.cat>
2026-04-18 20:08:00 +0200

    exercici2

---
+++ APIREST/services/user.py
@@ -0,0 +1,39 @@
+from config import database
+
+#conn = database.connection_db()
+
+def create_table():
+    conn = database.connection_db()
+    cursor = conn.cursor()
+    sql_table = '''
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            nombre VARCHAR(100) NOT NULL,
+            apellido VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            descripcion TEXT,
+            curso VARCHAR(100) NOT NULL,
+            anio VARCHAR(100) NOT NULL,
+            direccion VARCHAR(200) NOT NULL,
+            cp VARCHAR(10),
+            password VARCHAR(255) NOT NULL
+        )    
+    '''
+    cursor.execute(sql_table)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Tabla creada correctamente")
+    return {"mensaje": "Tabla creada correctamente"}
+
+def create_user(nombre, apellido, email, descripcion, curso, anio, direccion, cp, password):
+    conn = database.connection_db()
+    cursor = conn.cursor()
+    sql_insert = '''INSERT INTO users (nombre, apellido, email, descripcion, curso, anio, direccion, cp, password)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+    values = (nombre, apellido, email, descripcion, curso, anio, direccion, cp, password)
+    cursor.execute(sql_insert, values)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return {"mensaje": "Usuario creado correctamente"}
