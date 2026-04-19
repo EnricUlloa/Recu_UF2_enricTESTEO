6a1bd37c57e3c1ef50a2ac80eda08c8716c2e392
eulloa23 <eulloa23@ilg.cat>
2026-04-19 12:48:34 +0200

    exercici4

--- APIREST/schema/users_sch.py
+++ APIREST/schema/users_sch.py
@@ -1,2 +1,15 @@
-import
-#de momento este archivo esta vacio
+# Función que transforma UNA fila en JSON
+def user_schema(user):
+    return {
+        "id": user[0],
+        "nombre": user[1],
+        "apellido": user[2],
+        "email": user[3],
+        "curso": user[4],
+        "anio": user[5],
+        "direccion": user[6]
+    }
+
+# Función que itera una lista de filas y las transforma
+def users_schema(users):
+    return [user_schema(user) for user in users]
